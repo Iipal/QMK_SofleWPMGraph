40c17e31069c8a49ef96eab5c36c02ee24a1df63
Iipal <gtarasmaluh@gmail.com>
2022-05-02 17:14:25 +0300

    added Graph WPM, layout and compiled hex

---
+++ keymaps/via/oled.c
@@ -0,0 +1,162 @@
+/* Copyright 2020 Josef Adamcik
+ * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Sets up what the OLED screens display.
+
+#ifdef OLED_ENABLE
+
+int  timer = 0;
+char wpm_text[5];
+int  x          = 31;
+int  currwpm    = 0;
+int  vert_count = 0;
+
+//=============  USER CONFIG PARAMS  ===============
+float max_wpm                  = 150.0f; // WPM value at the top of the graph window
+int   graph_refresh_interval   = 80;     // in milliseconds
+int   graph_line_thickness     = 3;      // determines thickness of graph line in pixels
+int   graph_area_fill_interval = 1;      // determines how dense the horizontal lines under the graph line are lower = more dense
+int   vert_interval            = 3;      // determines frequency of vertical lines under the graph line
+bool  vert_line                = false;  // determines whether to draw vertical lines
+//=============  END USER PARAMS  ===============
+
+static inline void render_wpm(void) {
+    // get current WPM value
+    // oled_write_ln_P(PSTR("QWRT"), false);
+    currwpm = get_current_wpm();
+
+    // check if it's been long enough before refreshing graph
+    if (timer_elapsed(timer) > graph_refresh_interval) {
+        // main calculation to plot graph line
+        x = 32 - ((currwpm / max_wpm) * 32);
+
+        // first draw actual value line
+        for (int i = 0; i <= graph_line_thickness - 1; i++) {
+            oled_write_pixel(1, x + i, true);
+        }
+
+        // then fill in area below the value line
+        if (vert_line) {
+            if (vert_count == vert_interval) {
+                vert_count = 0;
+                while (x <= 32) {
+                    oled_write_pixel(1, x, true);
+                    x++;
+                }
+            } else {
+                for (int i = 32; i > x; i--) {
+                    if (i % graph_area_fill_interval == 0) {
+                        oled_write_pixel(1, i, true);
+                    }
+                }
+                vert_count++;
+            }
+        } else {
+            for (int i = 32; i > x; i--) {
+                if (i % graph_area_fill_interval == 0) {
+                    oled_write_pixel(1, i, true);
+                }
+            }
+        }
+
+        // then move the entire graph one pixel to the right
+        oled_pan(false);
+
+        // refresh the timer for the next iteration
+        timer = timer_read();
+    }
+
+    // format current WPM value into a printable string
+    itoa(currwpm, wpm_text, 10);
+
+    // formatting for triple digit WPM vs double digits, then print WPM readout
+    if (currwpm >= 100) {
+        oled_set_cursor(14, 3);
+        oled_write("WPM: ", false);
+        oled_set_cursor(18, 3);
+        oled_write(wpm_text, false);
+    } else if (currwpm >= 10) {
+        oled_set_cursor(15, 3);
+        oled_write("WPM: ", false);
+        oled_set_cursor(19, 3);
+        oled_write(wpm_text, false);
+    } else if (currwpm > 0) {
+        oled_set_cursor(16, 3);
+        oled_write("WPM: ", false);
+        oled_set_cursor(20, 3);
+        oled_write(wpm_text, false);
+    } else {
+        oled_set_cursor(16, 3);
+        oled_write("WPM:0", false);
+    }
+}
+
+static inline void print_status_narrow(void) {
+    // Print current mode
+    oled_write_P(PSTR("\n\n"), false);
+
+    const uint8_t layer_idx = get_highest_layer(layer_state);
+
+    switch (layer_idx) {
+        case 0:
+            oled_write_ln_P(PSTR("QWRT"), false);
+            break;
+        case 1:
+            oled_write_ln_P(PSTR("GAME"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("LYR"), false);
+            break;
+    }
+
+    oled_write_P(PSTR("\n\n"), false);
+
+    switch (layer_idx) {
+        case 0:
+        case 1:
+            oled_write_ln_P(PSTR("Base"), false);
+            break;
+        case 2:
+            oled_write_ln_P(PSTR("RSE"), false);
+            break;
+        case 3:
+            oled_write_ln_P(PSTR("LWR"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Undef"), false);
+            break;
+    }
+}
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_270;
+    }
+    return OLED_ROTATION_180;
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        print_status_narrow();
+    } else {
+        render_wpm();
+    }
+
+    return false;
+}
+
+#endif
---
+++ readme.md
@@ -0,0 +1,77 @@
+# This Fork Changes:
+
+### Major changes:
+
+```
+rules.mk:
+/**
+ * No more RGB lights suppports because with WPM_ENABLE.
+ * There is no enough memory on chip to enable both of this options.
+ **/
+RGBLIGHT_ENABLE = no
+WPM_ENABLE = yes
+
+config.h:
+# define OLED_TIMEOUT 20000 // now OLED screens offs after 20 seconds.
+# define OLED_BRIGHTNESS 50 // Max: 100
+# define SPLIT_WPM_ENABLE   // Enable WPM across split keyboards (+268).
+# define SPLIT_OLED_ENABLE  // Sync on/off OLED state between halves (+100).
+```
+
+### Left side texts:
+
+```
+Qwrt -> QWRT
+Clmk -> GAME
+Mod -> LYR
+
+Base -> *unchanged*
+Raise -> RSE
+Lower -> LWR
+
+CPSLK -> *removed*
+```
+
+### Right side:
+
+```
+WPM and typing graph.
+```
+
+### Compile:
+
+`qmk compile -km via`
+
+### Authority
+
+The graph code originally taken from [this Reddit post](https://www.reddit.com/r/nullbits/comments/iizlh2/wpm_graph_for_all_your_oled_screens/) with minor adaptation for Sofle v1\2.
+
+<br /><br /><br />
+
+# Sofle Keyboard
+
+![SofleKeyboard version 1](https://raw.githubusercontent.com/josefadamcik/SofleKeyboard/master/Images/IMG_20200126_114622.jpg)
+
+Sofle is 6×4+5 keys column-staggered split keyboard. Based on Lily58, Corne and Helix keyboards.
+
+More details about the keyboard on my blog: [Let me introduce you SofleKeyboard - a split keyboard based on Lily58 and Crkbd](https://josef-adamcik.cz/electronics/let-me-introduce-you-sofle-keyboard-split-keyboard-based-on-lily58.html)
+
+The current (temporary) build guide and a build log is available here: [SofleKeyboard build log/guide](https://josef-adamcik.cz/electronics/soflekeyboard-build-log-and-build-guide.html)
+
+- Keyboard Maintainer: [Josef Adamcik](https://josef-adamcik.cz) [Twitter:@josefadamcik](https://twitter.com/josefadamcik)
+- Hardware Supported: SofleKeyboard PCB, ProMicro
+- Hardware Availability: [PCB & Case Data](https://github.com/josefadamcik/SofleKeyboard)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make sofle:default
+
+Flash the default keymap:
+
+    make sofle:default:avrdude
+
+Press reset button on he keyboard when asked.
+
+Disconnect the first half, connect the second one and repeat the process.
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
